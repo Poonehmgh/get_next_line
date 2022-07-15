afc9ea2c0b61dc9aa9249e01a19986e4562da4e2
Poonehmgh <100726824+Poonehmgh@users.noreply.github.com>
2022-07-15 10:59:31 +0200

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: pmoghadd <pmoghadd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 21:13:14 by pmoghadd          #+#    #+#             */
-/*   Updated: 2022/06/13 20:11:57 by pmoghadd         ###   ########.fr       */
+/*   Updated: 2022/06/18 14:50:15 by pmoghadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
+#  define BUFFER_SIZE 432
 # endif
 
-# define MAXSIZE 20000
+# define MAXSIZE 200000
 # define FDMAX 1024
 
 # include <unistd.h>
@@ -29,7 +29,7 @@ void	ft_bzero(void *str, size_t n);
 size_t	ft_strlen(const char *s);
 char	*get_next_line(int fd);
 char	*ft_strchr(const char *s, int c);
-char	*ft_strcat(char *dst, const char *src);
+char	*ft_strcat(char *dst, char *src);
 char	*ft_strdup(const char *s);
 char	*ft_strcpy(char *dest, const char *src);
 
