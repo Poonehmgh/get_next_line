48c5cd0141a85550667b6ca22b88e50b5059dbad
Poonehmgh <100726824+Poonehmgh@users.noreply.github.com>
2022-07-15 10:57:52 +0200

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -53,6 +53,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	dst[i] = '\0';
 	return (dst);
 }
+
 //This function gets buffer and returns the part of buffer located after \n. it first copies it to copy string and then copies back from the right index.
 //if there is no \n, then it would return NULL. because it means that there is no part of buffer which is interesting for us and we are at end of the file.
 //since I am using strdup and it has malloc, i free the space when i am done.
@@ -74,7 +75,7 @@ char	*ft_updatebuf(char *buffer)
 	{
 		if (copy)
 			free(copy);
-		ft_bzero(buffer, ft_strlen(buffer));
+		ft_bzero(buffer, MAXSIZE);
 		return (buffer);
 	}	
 	if (copy)
@@ -86,13 +87,20 @@ char	*ft_updatebuf(char *buffer)
 // if there is no \n, it means the whole of this buffer should be returned as a line so it returns buf.
 // since the line always have to be allocated memory (I dont know why, and the subject file doesnt emphasize on that, but i understood it from the tripoullie tester behavior), when I return
 // buf, it is from an un-allocated memory. then the tester wants to free it and gives me extra freeing error. so, I use strdup, then an allocated memory will be returned and tester is fine with it!
+
 char	*ft_leftstr(char *buf, ssize_t *counter)
 {
 	char	*ptr;
 	char	*leftstr;
+	size_t	i;
 
+	i = *counter;
 	leftstr = NULL;
-	buf[*counter] = '\0';
+	while (i < MAXSIZE)
+	{
+		buf[i] = '\0';
+		i++;
+	}
 	ptr = ft_strchr(buf, '\n');
 	if (ptr)
 		leftstr = ft_substr(buf, 0, ft_strlen(buf) - ft_strlen(ptr) + 1);
@@ -107,7 +115,9 @@ char	*ft_leftstr(char *buf, ssize_t *counter)
 //you don't have to free stuff. Also it doesn't return value so you dont have to deal with other variables.
 //then I check if we are end of the file, program returns null. otherwise, the ft_leftstr function extracts the line part and the ft_updatebuf function updates buffer.
 // the point is, it is pretty necessary to null terminate the buf2 wherever it has no value from the read file in it, otherwise the other functions wont know when to stop searching through the buffer. 
-//thats why I have a counter variable and I count the number of read chars. So I pass it to ft_leftstr to null terminate the buf2 at the exact point. (I could have done that in the below function but the limitation of norminete didn't allow me!) 
+//thats why I have a counter variable and I count the number of read chars. So I pass it to ft_leftstr to null terminate the buf2 at the exact point. (I could have done that in the below 
+//function but the limitation of norminete didn't allow me!) 
+
 char	*get_next_line(int fd)
 {
 	char			buf[BUFFER_SIZE + 1];
@@ -121,7 +131,7 @@ char	*get_next_line(int fd)
 		return (NULL);
 	ft_bzero(buf, BUFFER_SIZE + 1);
 	n = 1;
-	while (!ft_strchr(buf, '\n') && n)
+	while (!ft_strchr(buf2, '\n') && n)
 	{
 		n = read(fd, buf, BUFFER_SIZE);
 		if (n < 0)
@@ -130,7 +140,7 @@ char	*get_next_line(int fd)
 		if (n)
 			ft_strcat(buf2, buf);
 	}
-	if ((n == 0 && !buf2[0]))
+	if ((n == 0 && !buf[0]) && !buf2[0])
 		return (0);
 	line = ft_leftstr(buf2, &counter);
 	ft_updatebuf(buf2);
